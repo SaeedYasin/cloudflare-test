e78e229e1bdd076e8795b66e33f198a1bdc39157
Saeed Yasin <saeedyasin@gmail.com>
2022-07-12 19:25:04 +0800

    Add precompress option in adapter

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      precompress: true
+    }),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
