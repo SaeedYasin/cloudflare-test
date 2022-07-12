3728a7cd21342aad2bc67b588125ff511bf91a0e
Saeed Yasin <saeedyasin@gmail.com>
2022-07-12 20:04:51 +0800

    Add path in vite vonfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from 'vite-imagetools';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [imagetools(), sveltekit()]
+	plugins: [sveltekit(), imagetools()],
+	resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib')
+    }
+  }
 };
 
 export default config;
