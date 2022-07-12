69a21f76b640bda8957f6605ea5d2d1630c3e5e4
Saeed Yasin <saeedyasin@gmail.com>
2022-07-12 22:16:09 +0800

    Use new styled layout

---
+++ src/routes/__layout-home@default.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // import Home from '$lib/home/index.svelte';
+</script>
+
+<main>
+  <slot />
+  <!-- <Home /> -->
+</main>
+
+<style lang="scss">
+  @use './src/scss/base' as *;
+
+  main {
+    @include flex_center;
+  }
+</style>
